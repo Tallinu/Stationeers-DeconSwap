82d8eadf0d5de040172f704b46952117e0763c27
Tallinu <yancarlo@gmail.com>
2025-10-12 03:03:40 -0700

    Logging code cleanup

--- DeconSwapPatch.cs
+++ DeconSwapPatch.cs
@@ -1,8 +1,10 @@
 ﻿using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Pipes;
 using HarmonyLib;
 using JetBrains.Annotations;
 using LaunchPadBooster.Utils;
 using Objects.Electrical;
+using Objects.Rockets;
 using Objects.Structures;
 
 namespace DeconSwap
@@ -32,7 +34,7 @@ namespace DeconSwap
                 if (thing is Frame frame && thing.PrefabName.StartsWith("StructureFrame"))
                 {
                     //Frames are handled here.
-                    DeconSwapPlugin.logger.LogInfo("Found frame " + frame.PrefabName + " with " + frame.BuildStates.Count + " build state" + (frame.BuildStates.Count != 1 ? "s." : "."));
+                    LogFoundStructure("frame", thing as Structure);
                     SwapAllTools(frame, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                     if (frame.BuildStates.Count > 2 && DeconSwapPlugin.grindTimeMultiplier.Value > 100)
                     {
@@ -50,59 +52,58 @@ namespace DeconSwap
                       && !thing.PrefabName.StartsWith("StructureCompositeWallType") /* Dismantled with crowbar */
                       && !thing.PrefabName.StartsWith("StructureCompositeFloorGrating")) /* Dismantled with crowbar */
                 {
-                    if (thing is Wall wall)
+                    if (thing is Wall)
                     {
                         //Normal walls of all kinds, and 2 of 3 window shutter pieces get handled here.
-                        DeconSwapPlugin.logger.LogInfo("Found wall " + wall.PrefabName + " with " + wall.BuildStates.Count + " build state" + (wall.BuildStates.Count != 1 ? "s." : "."));
-                        SwapAllTools(wall, PrefabNames.Wrench, PrefabNames.AngleGrinder);
+                        LogFoundStructure("wall", thing as Structure);
+                        SwapAllTools(thing as Structure, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                     }
-                    else if (thing is WallPillar wallp)
+                    else if (thing is WallPillar)
                     {
                         //These don't use the Wall class and have to be handled separately
-                        DeconSwapPlugin.logger.LogInfo("Found pillar " + wallp.PrefabName + " with " + wallp.BuildStates.Count + " build state" + (wallp.BuildStates.Count != 1 ? "s." : "."));
-                        SwapAllTools(wallp, PrefabNames.Wrench, PrefabNames.AngleGrinder);
+                        LogFoundStructure("pillar", thing as Structure);
+                        SwapAllTools(thing as Structure, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                     }
                 }
-                else if (thing is WindowShutterController wsc)
+                else if (thing is WindowShutterController)
                 {
                     //Special case to swap this even though it's not a Wall class, because all window shutter parts are made from the same kit,
                     //and all of their Build State 0's are normally dismantled with the same tool (grinder) just like regular walls are.
-                    int count = wsc.BuildStates.Count;
-                    DeconSwapPlugin.logger.LogInfo("Found wall " + wsc.PrefabName + " with " + count + " build state" + (count != 1 ? "s." : "."));
-                    SwapAllTools(wsc, PrefabNames.Wrench, PrefabNames.AngleGrinder);
+                    LogFoundStructure("wall", thing as Structure);
+                    SwapAllTools(thing as Structure, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                 }
-                else if ((thing is LandingPadModular pad) && thing.PrefabName.StartsWith("Landingpad_") && !thing.PrefabName.Equals("Landingpad_2x2CenterPiece01"))
+                else if ((thing is LandingPadModular) && thing.PrefabName.StartsWith("Landingpad_") && !thing.PrefabName.Equals("Landingpad_2x2CenterPiece01"))
                 {
                     //Landing pad parts also use grinder to pick up initial placement (except new large tank) and wrench to unweld steel sheets.
-                    DeconSwapPlugin.logger.LogInfo("Found pad " + pad.PrefabName + " with " + pad.BuildStates.Count + " build state" + (pad.BuildStates.Count != 1 ? "s." : "."));
-                    if (pad.PrefabName.Equals("Landingpad_GasCylinderTankPiece"))
+                    LogFoundStructure("pad", thing as Structure);
+                    if (thing.PrefabName.Equals("Landingpad_GasCylinderTankPiece"))
                     {
                         // Landingpad_GasCylinderTankPiece - Wrench (tank), Grinder (base) - Change BS 0 grinder to wrench, BS 1 wrench to drill
-                        SwapTools(pad, 0, PrefabNames.AngleGrinder, PrefabNames.Wrench);
-                        SwapTools(pad, 1, PrefabNames.Wrench, PrefabNames.Drill);
+                        SwapTools(thing as Structure, 0, PrefabNames.AngleGrinder, PrefabNames.Wrench);
+                        SwapTools(thing as Structure, 1, PrefabNames.Wrench, PrefabNames.Drill);
                     }
-                    else if (pad.PrefabName.Equals("Landingpad_LargeTank"))
+                    else if (thing.PrefabName.Equals("Landingpad_LargeTank"))
                     {
                         // Landingpad_LargeTank - Grinder (welded steel sheets, good), wrench (tanks), Hand Drill (base) - swap BS 0 and 1 (wrench, drill)
-                        SwapTools(pad, 0, PrefabNames.Drill, PrefabNames.Wrench);
-                        SwapTools(pad, 1, PrefabNames.Wrench, PrefabNames.Drill);
+                        SwapTools(thing as Structure, 0, PrefabNames.Drill, PrefabNames.Wrench);
+                        SwapTools(thing as Structure, 1, PrefabNames.Wrench, PrefabNames.Drill);
                     }
                     else
                     {
-                        SwapAllTools(pad, PrefabNames.Wrench, PrefabNames.AngleGrinder);
+                        SwapAllTools(thing as Structure, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                     }
                 }
-                else if (thing is LandingPadModularDevice paddev && thing.PrefabName.StartsWith("Landingpad_"))
+                else if (thing is LandingPadModularDevice && thing.PrefabName.StartsWith("Landingpad_"))
                 {
                     //'Data & Power' and runway 'Threshhold' (yes, with an extra H) are a separate class
-                    DeconSwapPlugin.logger.LogInfo("Found pad " + paddev.PrefabName + " with " + paddev.BuildStates.Count + " build state" + (paddev.BuildStates.Count != 1 ? "s." : "."));
-                    SwapAllTools(paddev, PrefabNames.Wrench, PrefabNames.AngleGrinder);
+                    LogFoundStructure("pad", thing as Structure);
+                    SwapAllTools(thing as Structure, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                 }
-                else if (thing is LandingPadPump padpump && thing.PrefabName.StartsWith("Landingpad_"))
+                else if (thing is LandingPadPump && thing.PrefabName.StartsWith("Landingpad_"))
                 {
                     //So are the gas/liquid in/out pump parts
-                    DeconSwapPlugin.logger.LogInfo("Found pad " + padpump.PrefabName + " with " + padpump.BuildStates.Count + " build state" + (padpump.BuildStates.Count != 1 ? "s." : "."));
-                    SwapAllTools(padpump, PrefabNames.Wrench, PrefabNames.AngleGrinder);
+                    LogFoundStructure("pad", thing as Structure);
+                    SwapAllTools(thing as Structure, PrefabNames.Wrench, PrefabNames.AngleGrinder);
                 }
                 else if (DeconSwapPlugin.extraLogOutputs.Value && thing is Structure struc)
                 {
@@ -135,9 +136,14 @@ namespace DeconSwap
             if (changeCount == expectedChanges)
                 DeconSwapPlugin.logger.LogInfo("Modified " + changeCount + " build states (this is the expected result).");
             else if (changeCount > 0)
-                DeconSwapPlugin.logger.LogWarning("Unexpected: Modified " + changeCount + " build states (expected: " + expectedChanges + "). Parts added by other mods or game updates may have been affected.");
+                DeconSwapPlugin.logger.LogWarning("Unexpected: Modified " + changeCount + " build states (expected: " + expectedChanges + "). Parts added by other mods or recent game updates may have been affected.");
             else
                 DeconSwapPlugin.logger.LogError("Unexpected: No build states were modified!");
+        }
+
+        private static void LogFoundStructure(string what, Structure struc)
+        {
+            DeconSwapPlugin.logger.LogInfo("Found " + what + ": " + struc.PrefabName + " with " + struc.BuildStates.Count + " build state" + (struc.BuildStates.Count != 1 ? "s." : "."));
         }
 
         private static void SwapTools(Structure structure, int state, string tool1, string tool2)
